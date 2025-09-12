05fb31de09340051fd2491b6f209dd640b3706e5
vc985077-ux <vc985077@gmail.com>
2025-09-12 21:38:03 +0000

    terminado

--- supertrunfo.c
+++ supertrunfo.c
@@ -3,14 +3,14 @@
 int main(){
     char Estado, estado_, pais1[30], Codigo1[4], Cidade1[50], pais[30], Codigo[4], Cidade[50];
     unsigned long int Populacao, Populacao1;
-    float Area, Pib, Area1, Pib1, densidade, capita, superPoder, densidade1, capita1, superPoder1;
+    float Area, Pib, Area1, Pib1, densidade, capita, superPoder, densidade1, capita1, superPoder1, resultado3, resultado4;
     int PontosTuristicos, PontosTuristicos1, escolha, escolha1, resultado1, resultado2;//Características
     
     printf("Carta 1: \n");
     printf("Digite o País: \n");
     scanf(" %s", pais);
 
-    printf("Digite o Estado (A - H): \n");
+    printf("Digite o Estado (Ex: A - H): \n");
     scanf(" %c", &Estado);
 
     printf("Digite o Código (Ex: 01): \n");
@@ -41,7 +41,7 @@ int main(){
     printf("Digite o País: \n");
     scanf(" %s", pais1);
     
-    printf("Digite o Estado (A - H): \n");
+    printf("Digite o Estado (Ex: A - H): \n");
     scanf(" %c", &estado_);
 
     printf("Digite o Código (Ex: 01): \n");
@@ -76,34 +76,34 @@ int main(){
     printf("3. PIB!\n");
     printf("4. Pontos Turísticos!\n");
     printf("5. Densidade Populacional!\n");
-    printf("\nEscolha sua opção: ");
+    printf("Escolha sua opção: ");
     scanf("%d", &escolha); //Apresentação Menu
 
     switch (escolha)
     {
      case 1:
-     printf(" Atributo escolhido: População\n");
-     resultado1 = Populacao > Populacao1 ? 1 : 0;
+     printf("\nAtributo escolhido: População\n");
+     resultado1 = (Populacao > Populacao1) ? 1 : 0;
         break;
     
      case 2:
-     printf(" Atributo escolhido: Área\n");
-     resultado1 = Area > Area1 ? 1 : 0;
+     printf("\nAtributo escolhido: Área\n");
+     resultado1 = (Area > Area1) ? 1 : 0;
         break;
     
         case 3:
-     printf(" Atributo escolhido: PIB\n");
-     resultado1 = Pib > Pib1 ? 1 : 0;
+     printf("\nAtributo escolhido: PIB\n");
+     resultado1 = (Pib > Pib1) ? 1 : 0;
         break;
     
         case 4:
-     printf(" Atributo escolhido: Pontos Turísticos\n");
-     resultado1 = PontosTuristicos > PontosTuristicos1 ? 1 : 0;
+     printf("\nAtributo escolhido: Pontos Turísticos\n");
+     resultado1 = (PontosTuristicos > PontosTuristicos1) ? 1 : 0;
         break;
     
         case 5:
-     printf(" Atributo escolhido: Densidade Populacional\n");
-     resultado1 = densidade < densidade1 ? 1 : 0;
+     printf("\nAtributo escolhido: Densidade Populacional\n");
+     resultado1 = (densidade < densidade1) ? 1 : 0;
         break; //Densidade Lógica Invertida
     
         default:
@@ -117,55 +117,375 @@ int main(){
     printf("3. PIB!\n");
     printf("4. Pontos Turísticos!\n");
     printf("5. Densidade Populacional!\n");
-    printf("\nEscolha sua opção: ");
+    printf("Escolha sua opção: ");
     scanf("%d", &escolha1);
-    
-    if (escolha == escolha1) {
-      printf("\nVocê não pode escolher o mesmo atributo!\n");
-    } else {
-    
-      switch (escolha1)
+
+    switch (escolha1)
     {
      case 1:
-     printf(" Atributo escolhido: População\n");
-     resultado2 = Populacao > Populacao1 ? 1 : 0;
+     printf("\nAtributo escolhido: População\n");
+     
+     if (escolha == escolha1)
+    {
+      printf("\nVocê não pode escolher o mesmo atributo!\n");
+     } 
+     resultado2 = (Populacao > Populacao1) ? 1 : 0;
         break;
     
      case 2:
-     printf("Atributo escolhido: Área\n");
-     resultado2 = Area > Area1 ? 1 : 0;
+     printf("\nAtributo escolhido: Área\n");
+
+   if (escolha == escolha1)
+    {
+      printf("\nVocê não pode escolher o mesmo atributo!\n");
+    }
+    resultado2 = (Area > Area1) ? 1 : 0;
         break;
     
         case 3:
-     printf(" Atributo escolhido: PIB\n");
-     resultado2 = Pib > Pib1 ? 1 : 0;
+     printf("\nAtributo escolhido: PIB\n");
+
+     if (escolha == escolha1) {
+      printf("\nVocê não pode escolher o mesmo atributo!\n");
+    }
+    resultado2 = (Pib > Pib1) ? 1 : 0;
         break;
     
         case 4:
-     printf(" Atributo escolhido: Pontos Turísticos\n"); 
-     resultado2 = PontosTuristicos > PontosTuristicos1 ? 1 : 0;
+     printf("\nAtributo escolhido: Pontos Turísticos\n");
+
+     if (escolha == escolha1)
+    {
+      printf("\nVocê não pode escolher o mesmo atributo!\n");
+    } 
+    resultado2 = (PontosTuristicos > PontosTuristicos1) ? 1 : 0;
         break;
     
         case 5:
-     printf(" Atributo escolhido: Densidade Populacional\n");
-     resultado1 = densidade < densidade1 ? 1 : 0;
+     printf("\nAtributo escolhido: Densidade Populacional\n");
+     if (escolha == escolha1) {
+      printf("\nVocê não pode escolher o mesmo atributo!\n");
+    } 
+    resultado2 = (densidade < densidade1) ? 1 : 0;
         break; //Densidade Lógica Invertida
    
        default:
      printf("Opção Inválida!\n");
     }
 
-    if (resultado1 == resultado2)
+    printf("\n*** Resultados ***\n"); //Exibição Resultados
+    printf("Carta 1: %s, %s\n", Cidade, pais);
+    printf("Carta 2: %s, %s\n", Cidade1, pais1);
+
+    escolha == 1 && escolha1 == 2 ? printf("\nAtributos usados: População e Área!\n") : 0;
+    resultado3 = (float)Populacao + Area;
+    resultado4 = (float)Populacao1 + Area1;
+    if (escolha == 1 && escolha1 == 2)
+    {
+      printf("\nPopulação de %s é: %lu\n", Cidade, Populacao);
+      printf("População de %s é: %lu\n", Cidade1, Populacao1);
+
+      printf("\nÁrea de %s é: %.3f\n", Cidade, Area);
+      printf("Área de %s é: %.3f\n", Cidade1, Area1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 1 && escolha1 == 3 ? printf("\nAtributos usados: População e PIB!\n") : 0;
+    resultado3 = (float)Populacao + Pib;
+    resultado4 = (float)Populacao1 + Pib1;
+    if (escolha == 1 && escolha1 == 3)
+    {
+      printf("\nPopulação de %s é: %lu\n", Cidade, Populacao);
+      printf("População de %s é: %lu\n", Cidade1, Populacao1);
+
+      printf("\nPIB de %s é: %.3f\n", Cidade, Pib);
+      printf("PIB de %s é: %.3f\n", Cidade1, Pib1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    escolha == 1 && escolha1 == 4 ? printf("\nAtributos usados: População e Pontos Turísticos!\n") : 0;
+    resultado3 = (float)Populacao + PontosTuristicos;
+    resultado4 = (float)Populacao1 + PontosTuristicos1;
+    if (escolha == 1 && escolha1 == 4)
+    {
+      printf("\nPopulação de %s é: %lu\n", Cidade, Populacao);
+      printf("População de %s é: %lu\n", Cidade1, Populacao1);
+
+      printf("\nPontos Turísticos de %s é: %d\n", Cidade, PontosTuristicos);
+      printf("Pontos Turísticos de %s é: %d\n", Cidade1, PontosTuristicos1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    escolha == 1 && escolha1 == 5 ? printf("\nAtributos usados: Polução e Densidade Populacional!\n") : 0;
+    resultado3 = (float)Populacao + PontosTuristicos;
+    resultado4 = (float)Populacao1 + PontosTuristicos1;
+    if (escolha == 1 && escolha1 == 5)
+    {
+      printf("\nPopulação de %s é: %lu\n", Cidade, Populacao);
+      printf("População de %s é: %lu\n", Cidade1, Populacao1);
+
+      printf("\nDensidade Populacional de %s é: %.3f\n", Cidade, densidade);
+      printf("Densidade Populacional de %s é: %.3f\n", Cidade1, densidade1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+
+    escolha == 2 && escolha1 == 1 ? printf("\nAtributos usdaos: Área e População!\n") : 0;
+    resultado3 = (float)Populacao + Area;
+    resultado4 = (float)Populacao1 + Area1;
+    if (escolha == 2 && escolha1 == 1)
+    {
+      printf("\nÁrea de %s é: %.3f\n", Cidade, Area);
+      printf("Área de %s é: %.3f\n", Cidade1, Area1);
+
+      printf("\nPopulação de %s é: %lu\n", Cidade, Populacao);
+      printf("População de %s é: %lu\n", Cidade1, Populacao1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    escolha == 2 && escolha1 == 3 ? printf("\nAtributos usados: Área e PIB!\n") : 0;
+    resultado3 = (float)Area + Pib;
+    resultado4 = (float)Area1 + Pib1;
+    if (escolha == 2 && escolha1 == 3)
+    {
+      printf("\nÁrea de %s é: %.3f\n", Cidade, Area);
+      printf("Área de %s é: %.3f\n", Cidade1, Area1);
+
+      printf("\nPIB de %s é: %.3f\n", Cidade, Pib);
+      printf("PIB de %s é: %.3f\n", Cidade1, Pib1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 2 && escolha1 == 4 ? printf("\nAtributos usados: Área e Pontos Turísticos!\n") : 0;
+    resultado3 = (float)Area + PontosTuristicos;
+    resultado4 = (float)Area1 + PontosTuristicos1;
+    if (escolha == 2 && escolha1 == 4)
+    {
+      printf("\nÁrea de %s é: %.3f\n", Cidade, Area);
+      printf("Área de %s é: %.3f\n", Cidade1, Area1);
+
+      printf("\nPontos Turísticos de %s é: %d\n", Cidade, PontosTuristicos);
+      printf("Pontos Turísticos de %s é: %d\n", Cidade1, PontosTuristicos1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 2 && escolha1 == 5 ? printf("\nAtributos usados: Área e Densidade Populacional!\n") : 0;
+    resultado3 = (float)Area + densidade;
+    resultado4 = (float)Area1 + densidade1;
+    if (escolha == 2 && escolha1 == 5)
+    {
+      printf("\nÁrea de %s é: %.3f\n", Cidade, Area);
+      printf("Área de %s é: %.3f\n", Cidade1, Area1);
+
+      printf("\nDensidade Populacional de %s é: %.3f\n", Cidade, densidade);
+      printf("Densidade Populacional de %s é: %.3f\n", Cidade1, densidade1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 3 && escolha1 == 1 ? printf("\nAtributos usdaos: PIB e População!\n") : 0;
+    resultado3 = (float)Pib + Populacao;
+    resultado4 = (float)Pib1 + Populacao1;
+    if (escolha == 3 && escolha1 == 1)
+    {
+      printf("\nPIB de %s é: %.3f\n", Cidade, Pib);
+      printf("PIB de %s é: %.3f\n", Cidade1, Pib1);
+
+      printf("\nPopulação de %s é: %lu\n", Cidade, Populacao);
+      printf("População de %s é: %lu\n", Cidade1, Populacao1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 3 && escolha1 == 2 ? printf("\nAtributos usdaos: PIB e Área!\n") : 0;
+    resultado3 = (float)Pib + Area;
+    resultado4 = (float)Pib1 + Area1;
+    if (escolha == 3 && escolha1 == 2)
+    {
+      printf("\nPIB de %s é: %.3f\n", Cidade, Pib);
+      printf("PIB de %s é: %.3f\n", Cidade1, Pib1);
+
+      printf("\nÁrea de %s é: %.3f\n", Cidade, Area);
+      printf("Área de %s é: %.3f\n", Cidade1, Area1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 3 && escolha1 == 4 ? printf("\nAtributos usdaos: PIB e Pontos Turísticos!\n") : 0;
+    resultado3 = (float)Pib + PontosTuristicos;
+    resultado3 = (float)Pib + PontosTuristicos1;
+    if (escolha == 3 && escolha1 == 4)
+    {
+      printf("\nPIB de %s é: %.3f\n", pais, Pib);
+      printf("PIB de %s é: %.3f\n", pais1, Pib1);
+
+      printf("\nPontos Turísticos de %s é: %d\n", Cidade, PontosTuristicos);
+      printf("Pontos Turísticos de %s é: %d\n", Cidade1, PontosTuristicos1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 3 && escolha1 == 5 ? printf("\nAtributos usdaos: PIB e Densidade Populacional!\n") : 0;
+    resultado3 = (float)Pib + densidade;
+    resultado4 = (float)Pib1 + densidade1;
+    if (escolha == 3 && escolha1 == 5)
+    {
+      printf("\nPIB de %s é: %.3f\n", pais, Pib);
+      printf("PIB de %s é: %.3f\n", pais1, Pib1);
+
+      printf("\nDensidade Populacional de %s é: %.3f\n", Cidade, densidade);
+      printf("Densidade Populacional de %s é: %.3f\n", Cidade1, densidade1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+
+    escolha == 4 && escolha1 == 1 ? printf("\nAtributos usados: Pontos Turísticos e População!\n") : 0;
+    resultado3 = (float)PontosTuristicos + Populacao;
+    resultado4 = (float)PontosTuristicos1 + Populacao1;
+    if (escolha == 4 && escolha1 == 1)
+    {
+      printf("\nPontos Turísticos de %s é: %d\n", Cidade, PontosTuristicos);
+      printf("Pontos Turísticos de %s é: %d\n", Cidade1, PontosTuristicos1);
+
+      printf("\nPopulação de %s é: %lu\n", Cidade, Populacao);
+      printf("População de %s é: %lu\n", Cidade1, Populacao1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 4 && escolha1 == 2 ? printf("\nAtributos usados: Pontos Turísticos e Área!\n") : 0;
+    resultado3 = (float)PontosTuristicos + Area;
+    resultado4 = (float)PontosTuristicos1 + Area1;
+    if (escolha == 4 && escolha1 == 2)
+    {
+      printf("\nPontos Turísticos de %s é: %d\n", Cidade, PontosTuristicos);
+      printf("Pontos Turísticos de %s é: %d\n", Cidade1, PontosTuristicos1);
+
+      printf("\nÁrea de %s é: %.3f\n", cidade, Area);
+      printf("Área de %s é: %.3f\n", Cidade1, Area1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 4 && escolha1 == 3 ? printf("\nAtributos usados: Pontos Turísticos e PIB!\n") : 0;
+    resultado3 = (float)PontosTuristicos + Pib;
+    resultado4 = (float)PontosTuristicos1 + Pib1;
+    if (escolha == 4 && escolha1 == 3)
+    {
+      printf("\nPontos Turísticos de %s é: %d\n", Cidade, PontosTuristicos);
+      printf("Pontos Turísticos de %s é: %d\n", Cidade1, PontosTuristicos1);
+
+      printf("\nPIB de %s é: %.3f\n", Cidade, Pib);
+      printf("PIB de %s é: %.3f\n", Cidade1, Pib1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 4 && escolha1 == 5 ? printf("\nAtributos usados: Pontos Turísticos e Densidade Populacional!\n") : 0;
+    resultado3 = (float)PontosTuristicos + densidade;
+    resultado4 = (float)PontosTuristicos1 + densidade1;
+    if (escolha == 4 && escolha1 == 5)
+    {
+      printf("\nPontos Turísticos de %s é: %d\n", Cidade, PontosTuristicos);
+      printf("Pontos Turísticos de %s é: %d\n", Cidade1, PontosTuristicos1);
+
+      printf("\nDensidade Populacional de %s é: %.3f\n", Cidade, densidade);
+      printf("Densidade Populacional de %s é: %.3f\n", Cidade1, densidade1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+
+    escolha == 5 && escolha1 == 1 ? printf("\nAtributos usados: Densidade Populacional e População!\n") : 0;
+    resultado3 = (float)densidade + Populacao;
+    resultado4 = (float)densidade1 + Populacao1;
+    if (escolha == 5 && escolha1 == 1)
+    {
+      printf("\nDensidade Populacional de %s é: %.3f\n", Cidade, densidade);
+      printf("Densidade Populacional de %s é: %.3f\n", Cidade1, densidade1);
+
+      printf("\nPopulação de %s é: %lu\n", Cidade, Populacao);
+      printf("População de %s é: %lu\n", Cidade1, Populacao1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 5 && escolha1 == 2 ? printf("\nAtributos usados: Densidade Populacional e Área!\n") : 0;
+    resultado3 = (float)densidade + Area;
+    resultado4 = (float)densidade1 + Area1;
+    if (escolha == 5 && escolha1 == 2)
+    {
+      printf("\nDensidade Populacional de %s é: %.3f\n", Cidade, densidade);
+      printf("Densidade Populacional de %s é: %.3f\n", Cidade1, densidade1);
+
+      printf("\nÁrea de %s é: %.3f\n", Cidade, Area);
+      printf("Área de %s é: %.3f\n", Cidade1, Area1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 5 && escolha1 == 3 ? printf("\nAtributos usados: Densidade Populacional e PIB!\n") : 0;
+    resultado3 = (float)densidade + Pib;
+    resultado4 = (float)densidade1 + Pib1;
+
+    if (escolha == 5 && escolha1 == 3)
+    {
+      printf("\nDensidade Populacional de %s é: %.3f\n", Cidade, densidade);
+      printf("Densidade Populacional de %s é: %.3f\n", Cidade1, densidade1);
+
+      printf("\nPIB de %s é: %.3f\n", Cidade, Pib);
+      printf("PIB de %s é: %.3f\n", Cidade1, Pib1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    }
+    
+    escolha == 5 && escolha1 == 4 ? printf("\nAtributos usados: Densidade Populacional e Pontos Turísticos!\n") : 0;
+    resultado3 = (float)densidade + PontosTuristicos;
+    resultado4 = (float)densidade1 + PontosTuristicos1;
+    if (escolha == 5 && escolha1 == 4)
+    {
+      printf("\nDensidade Populacional de %s é: %.3f\n", Cidade, densidade);
+      printf("Densidade Populacional de %s é: %.3f\n", Cidade1, densidade1);
+
+      printf("\nPontos Turísticos de %s é: %d\n", Cidade, PontosTuristicos);
+      printf("Pontos Turísticos de %s é: %d\n", Cidade1, PontosTuristicos1);
+
+      printf("\nA soma dos atributos usados para Carta 1 é: %.3f\n", resultado3);
+      printf("A soma dos atributos usados para Carta 2 é: %.3f\n", resultado4);
+    } //Exibição Resultados
+
+    if (resultado3 == resultado4)
     {
       printf("\n** Empatou! **\n");
-    } else if (resultado1 == 1 && resultado2 == 0) { 
+    } else if (resultado3 > resultado4) { 
       printf("\n*** Parabéns, Carta 1 venceu! ***\n");
     } else {
       printf("\n*** Parabéns, Carta 2 venceu! ***\n");
     }
 
-   }
-
     return 0;
-   }
 
+}
